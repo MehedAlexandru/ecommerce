469009535a11b0efba304d9feb1279c0c5c8a636
Dio <alex.mehedinteanu.95@gmail.com>
2023-11-11 07:49:09 +0200

    Modified the related_name in the models.py file for bids and comments

--- auctions/models.py
+++ auctions/models.py
@@ -12,18 +12,19 @@ class Auction_listings(models.Model):
     starting_bid = models.IntegerField()
     image_url = models.URLField(blank=True)
     category = models.CharField(max_length=64, blank=True)
+    active = models.BooleanField(default=True)
     def __str__(self) -> str:
-        return f"{self.title}, {self.description}, {self.starting_bid}, {self.image_url}, {self.category}" 
+        return f"{self.title}, {self.description}, {self.starting_bid}, {self.image_url}, {self.category}, {self.active}" 
 
 class Bids(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
     auction = models.ForeignKey(Auction_listings, on_delete=models.CASCADE, related_name="bids")
     bid = models.IntegerField()
     def __str__(self) -> str:
         return f"{self.user}, {self.auction}, {self.bid}"
 
 class Comments(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
     auction = models.ForeignKey(Auction_listings, on_delete=models.CASCADE, related_name="comments")
     comment = models.TextField()
     def __str__(self) -> str:
