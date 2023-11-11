1a8366010019eb87c9fe26c282a14250fb4e5857
Dio <alex.mehedinteanu.95@gmail.com>
2023-11-11 07:10:03 +0200

    Initial commit. The database was designed and implemented. No other  changes

---
+++ auctions/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Auction_listings(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctions")
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    image_url = models.URLField(blank=True)
+    category = models.CharField(max_length=64, blank=True)
+    def __str__(self) -> str:
+        return f"{self.title}, {self.description}, {self.starting_bid}, {self.image_url}, {self.category}" 
+
+class Bids(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    auction = models.ForeignKey(Auction_listings, on_delete=models.CASCADE, related_name="bids")
+    bid = models.IntegerField()
+    def __str__(self) -> str:
+        return f"{self.user}, {self.auction}, {self.bid}"
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    auction = models.ForeignKey(Auction_listings, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField()
+    def __str__(self) -> str:
+        return f"{self.user}, {self.auction}, {self.comment}"
